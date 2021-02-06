d085718a836a319c1b914a413314855f3a1cfd6c
Fabio Guenci <guenci@uirnet.it>
2021-02-06 19:35:10 +0100

    model

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -23,7 +23,7 @@ import tensorflow.keras.utils as KU
 from tensorflow.python.eager import context
 import tensorflow.keras.models as KM
 
-from mrcnn import utils
+from comics_panel_detection.mrcnn import utils
 
 # Requires TensorFlow 2.0+
 from distutils.version import LooseVersion
