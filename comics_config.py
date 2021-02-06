d2bc8b5e1acce60e8754a205a6d5a0666415101f
Fabio Guenci <guenci@uirnet.it>
2021-02-06 19:29:54 +0100

    gpu

--- comics_config.py
+++ comics_config.py
@@ -7,11 +7,11 @@ class ComicsConfig(Config):
     # Number of classes (background + kangaroo)
     NUM_CLASSES = 1 + 2
     # Number of training steps per epoch
-    STEPS_PER_EPOCH = 99
+    STEPS_PER_EPOCH = 170
     # NUMBER OF GPUs to use. When using only a CPU, this needs to be set to 1.
     GPU_COUNT = 1
     # Number of images to train with on each GPU. A 12GB GPU can typically
     # handle 2 images of 1024x1024px.
     # Adjust based on your GPU memory and image sizes. Use the highest
     # number that your GPU can handle for best performance.
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 2
